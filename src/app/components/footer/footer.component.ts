1ed15190606b79bfd6f0b9c55f22d009eb5e1afd
ManuPunk16 <43177814+ManuPunk16@users.noreply.github.com>
2023-06-02 12:40:58 -0600

    Paypal Donations

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var window: any;
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    const script = document.createElement('script');
+    script.src = 'https://www.paypalobjects.com/donate/sdk/donate-sdk.js';
+    script.charset = 'UTF-8';
+    script.onload = () => {
+      this.renderPayPalButton();
+    };
+    document.body.appendChild(script);
+  }
+
+  renderPayPalButton(): void {
+    const donateButtonContainer = document.getElementById('donate-button-container');
+    const donateButton = document.getElementById('donate-button');
+    const PayPal = window['PayPal'];
+
+    PayPal.Donation.Button({
+      env: 'production',
+      hosted_button_id: 'SDF5YWKLVH3TA',
+      image: {
+        src: 'https://www.paypalobjects.com/en_US/MX/i/btn/btn_donateCC_LG.gif',
+        alt: 'Donate with PayPal button',
+        title: 'PayPal - The safer, easier way to pay online!',
+      },
+    }).render('#donate-button');
+  }
 }
